21b29cddee92a6ab3cf63125c05abe3de654c75c
Erick Ortega <shut435@yahoo.com>
2024-04-23 16:15:15 -0400

    new js file to calculate area of a cirlce (exercise)

---
+++ workbook2/Day2/WB2-exercises/MathScripts/area_of_circle.js
@@ -0,0 +1,8 @@
+// Exercise 2 Part 4
+// Calculate area of a circle
+
+// A = pi(r)^2 
+var pi = Math.PI;
+var radius = 5;
+var area = pi * Math.pow(radius, 2);
+console.log("The area of a circle is " + area);
