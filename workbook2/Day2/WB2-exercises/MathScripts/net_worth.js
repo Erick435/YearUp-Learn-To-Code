192f1c866407ff4ad569a5163d37d66b48f56cdd
Erick Ortega <shut435@yahoo.com>
2024-04-23 16:00:19 -0400

    Created workbook2 folder and basic javascript files

---
+++ workbook2/Day2/WB2-exercises/MathScripts/net_worth.js
@@ -0,0 +1,8 @@
+// Exercise 1 part 1 
+// Calculate your given net worth and assets and debts
+
+var assets = 100000;
+var debts = 100;
+netWorth = assets - debts;
+
+console.log("Your net worth is: " + netWorth);
--- workbook2/Day2/scratchpad.js
+++ workbook2/Day2/scratchpad.js
@@ -1,5 +1,11 @@
-var box1 = "Hello World!";
-var box2 = 21;
+// var box1 = "Hello World!";
+// var box2 = 21;
 
-console.log(box1);
-console.log(box2);
+// console.log(box1);
+// console.log(box2);
+
+// Exercise 3 - Write variables and assign name, city and state you were born in, then log it
+var firstName = "Erik";
+var city = "Honolulu";
+var state = "Hawaii";
+console.log("My name is " + firstName + ", I was born in " + city + ", " + state);
