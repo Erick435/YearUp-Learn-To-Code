bc1d9d663241b130770cf691c036d660a333e299
Erick Ortega <shut435@yahoo.com>
2024-04-25 13:48:14 -0400

    completed exercise for if statement with greeting using time

---
+++ workbook2/WB2-exercises/IfScripts/greeting.js
@@ -0,0 +1,17 @@
+// Exercise 1 part 2 
+// Define variable that contians the current hour (0 - 23). Display the greetings below based on the current hour)
+
+var currentTime = 18; 
+
+if (currentTime <= 10){
+    console.log("Good morning!");
+}
+else if (currentTime >= 10 && currentTime < 17){
+    console.log("Good Day!");
+}
+else if (currentTime >= 17 && currentTime < 24){
+    console.log("Good Evening!");
+}
+else {
+    console.log("Sorry that's not a valid time, please use 0 - 23")
+}
