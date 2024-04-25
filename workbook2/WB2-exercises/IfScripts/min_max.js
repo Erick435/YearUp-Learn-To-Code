53d832c4374f1e1afaa9f967e158347cd9d6af2f
Erick Ortega <shut435@yahoo.com>
2024-04-25 14:54:55 -0400

    exercise to use if/else and determine different values

---
+++ workbook2/WB2-exercises/IfScripts/min_max.js
@@ -0,0 +1,20 @@
+// Exercise 3 Part 1
+// Create 3 variable with different values, use if/else to determine the answer
+
+var a = 1;
+var b = 5;
+var c = 10;
+
+// var answer = a;
+// var answer = b;
+var answer = c;
+
+if (answer < 5){
+    console.log("you have a number less than 5");
+}
+else if (answer >= 5 && answer <10){
+    console.log("you have a number greater than 5 and less than 10");
+}
+else {
+    console.log("you have a number greater than 10");
+}
