bc0d821de9bce79ba9a6f6c1eaf645b1d624ec15
Erick Ortega <shut435@yahoo.com>
2024-04-30 09:03:14 -0400

    created folder for new workbook and working on functions, completed exercise 1

---
+++ workbook3/Functions/simple_functions.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function favoriteThings(){
+    console.log("Erik");
+    console.log("UP");
+    console.log("Fall Out Boy");
+
+}
+
+function whyImHere(){
+    console.log("I'm to gain more experience and knowledge");
+}
+
+function favoritePlace(){
+    console.log("I would love to visit Japan and explore");
+}
+
+favoriteThings();
+
+whyImHere();
+
+favoritePlace();
