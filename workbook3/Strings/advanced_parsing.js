f8a14ad1da7712a0011e54bd5e2476a42ce546e4
Erick Ortega <shut435@yahoo.com>
2024-05-02 09:35:21 -0400

    updated advanced parsing file

--- workbook3/Strings/advanced_parsing.js
+++ workbook3/Strings/advanced_parsing.js
@@ -3,43 +3,65 @@
 "use strict";
 
 function parseAndDisplayName(nameOfPerson){
-    let separationOfName = nameOfPerson.indexOf(" ");
-    let separationOfName2 = nameOfPerson.indexOf(" ", separationOfName + 1);
     
-    let firstName, lastName, middleName, answer;
+    let names = nameOfPerson.split(" ");
+
+    let firstName = names[0];
+    let middleName = "";
+    let lastName = "";
+
+    if (names.length == 2){
+        lastName = names[1];
+    }
+    else if (names.length == 3) {
+        middleName = names[1];
+        lastName = names[2];
+    }
+
+    console.log(`\nName: ${nameOfPerson}`);
+    console.log(`First Name: ${firstName}`);
+    console.log(`Middle Name: ${middleName}`);
+    console.log(`Last Name: ${lastName}`);
+
+
     
+    
+    // let separationOfName = nameOfPerson.indexOf(" ");
+    // let separationOfName2 = nameOfPerson.indexOf(" ", separationOfName + 1);
+    
+    // if (separationOfName < 0){
+    //     firstName = nameOfPerson;
+    // }
+    // else {
+    //     firstName = nameOfPerson.substring(0, separationOfName);
+    //     lastName = nameOfPerson.substring(separationOfName + 1);
+    // }
 
-    if (separationOfName < 0){
-        firstName = nameOfPerson;
-    }
-    else {
-        firstName = nameOfPerson.substring(0, separationOfName);
-        lastName = nameOfPerson.substring(separationOfName + 1);
-    }
-
-    if (separationOfName2 > 0){
-        lastName = nameOfPerson.substring(separationOfName + 1);
+    // if (separationOfName2 > 0){
+    //     lastName = nameOfPerson.substring(separationOfName + 1);
         
-    }
-    else if (separationOfName2 > separationOfName){
-        middleName = nameOfPerson.substring(separationOfName + 1, separationOfName2)
-        lastName = nameOfPerson.substring(separationOfName2 + 1);
-    }
+    // }
+    // else if (separationOfName2 > separationOfName){
+    //     middleName = nameOfPerson.substring(separationOfName + 1, separationOfName2)
+    //     lastName = nameOfPerson.substring(separationOfName2 + 1);
+    // }
 
-    if (!middleName && !lastName){
-        answer = "First name: " + firstName;
-    }
-    else if (!middleName){
-        answer = "First name: " + firstName + "\nlast name: " + lastName;
-    }
-    else {
-        answer = "First name: " + firstName + "\nMiddle Name: " + middleName + "\nlast name: " + lastName;
-    }
+    // if (!middleName && !lastName){
+    //     answer = "First name: " + firstName;
+    // }
+    // else if (!middleName){
+    //     answer = "First name: " + firstName + "\nlast name: " + lastName;
+    // }
+    // else {
+    //     answer = "First name: " + firstName + "\nMiddle Name: " + middleName + "\nlast name: " + lastName;
+    // }
 
-    console.log("\n\n" + answer);
+    // console.log("\n\n" + answer);
+
+
 
 }
 
-let person1 = parseAndDisplayName("Cher");
-let person2 = parseAndDisplayName("Brenda Kaye");
-let person3 = parseAndDisplayName("Dana Lynn Wyatt");
+parseAndDisplayName("Cher");
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Dana Lynn Wyatt");
