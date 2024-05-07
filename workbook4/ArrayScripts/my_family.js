ee0bf0090d3a077a6c960e67a9d3142764b8c9f8
Erick Ortega <shut435@yahoo.com>
2024-05-07 13:10:33 -0400

    script to loop over an array

---
+++ workbook4/ArrayScripts/my_family.js
@@ -0,0 +1,11 @@
+// Exercise 1 declare an array with 4 names and loop and print them
+
+"use strict";
+
+let family = ["John", "Mary", "Joe", "Susan", "Erik"];
+
+for (let i = 0; i < family.length; i++) {
+
+    console.log(family[i]);
+}
+
