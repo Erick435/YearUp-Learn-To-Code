fb657e349e58abd7603dbb5517ab75be89294b50
Erick Ortega <shut435@yahoo.com>
2024-05-16 11:58:10 -0400

    practicing creating tables using js to the HTML

---
+++ Workbooks/workbook5/LotteryTickets/scripts/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+let winningTickets = [
+    { tixNum: "1001001", expires: "2022-09-05", prize: 100000 },
+    { tixNum: "1298711", expires: "2022-10-10", prize: 250000 },
+    { tixNum: "1309182", expires: "2022-12-30", prize: 500000 },
+    { tixNum: "1456171", expires: "2023-01-20", prize: 1000000 },
+    { tixNum: "3332871", expires: "2022-05-20", prize: 1000000 },
+    { tixNum: "4651529", expires: "2022-12-15", prize: 100000 },
+    { tixNum: "5019181", expires: "2023-01-31", prize: 250000 },
+    { tixNum: "5168261", expires: "2023-03-01", prize: 1000000 },
+    { tixNum: "6761529", expires: "2022-12-15", prize: 250000 },
+    { tixNum: "7778172", expires: "2023-01-15", prize: 5000000 },
+    { tixNum: "8751426", expires: "2020-09-15", prize: 100000 }
+];
+
+const winningTicketsTblBody = document.querySelector("#winningTicketsTblBody");
+
+window.onload = function(){
+
+    loadWinningTicketsTable();
+
+}
+
+function loadWinningTicketsTable() {
+    let wTickets = winningTickets;
+    for (let wTicket of wTickets){
+
+        // these 2 lines of code does the same thing as the one below
+        // let row = document.createElement("tr");
+        // winningTicketsTblBody.appendChild(row);
+
+        let row = winningTicketsTblBody.insertRow();
+
+        let cell1 = row.insertCell(0);
+        cell1.innerText = wTicket.tixNum;
+
+        let cell2 = row.insertCell(1);
+        cell2.innerText = "$" + wTicket.prize.toFixed(2);
+
+        let cell3 = row.insertCell(2);
+        cell3.innerText = wTicket.expires
+
+
+        
+    }
+}
