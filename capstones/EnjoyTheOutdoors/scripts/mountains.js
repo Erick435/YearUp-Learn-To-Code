7f0d93979213abc01ce063160b1ef9e02be9a212
Erick Ortega <shut435@yahoo.com>
2024-05-22 11:51:14 -0400

    created option to show all mountain cards and display them neatly

--- capstones/EnjoyTheOutdoors/scripts/mountains.js
+++ capstones/EnjoyTheOutdoors/scripts/mountains.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const dropDownSelect = document.querySelector("#dropDownSelect");
-const cardContainer = document.querySelector("#cardContainer");
+// const cardContainer = document.querySelector("#cardContainer");
+const mountainsPage = document.querySelector("#mountainsPage");
 
 let mountains = mountainsArray;
 
 
 window.onload = function() {
 
+    
     displayDropDownNames();
-
+    
+    displaySelectedMountain();
     dropDownSelect.onchange = displaySelectedMountain;
 
 
@@ -36,41 +39,91 @@ function displayDropDownNames(){
 function displaySelectedMountain(){
     console.log("displaySelectedMountain() was called ");
 
-    cardContainer.innerText = "";
+    mountainsPage.innerText = "";
     
     let selectedValue = dropDownSelect.value;
     let mountainCard = mountains.find(mountain => mountain.elevation == selectedValue);
     
-    let cardImg = document.createElement("img");
-    cardImg.classList.add("card-img-top");
-    cardImg.src = `./images/${mountainCard.img}`;
-    cardImg.alt = mountainCard.name;
 
-    cardContainer.appendChild(cardImg);
+    let mountainCardCreate = function (mountainArray) {
+
+        
+        let cardContainer = document.createElement("div");
+        cardContainer.classList.add("container");
+        cardContainer.classList.add("card");
+        cardContainer.classList.add("my-5");
+        cardContainer.style = "width: 25rem";
+        cardContainer.id = "cardContainer";
+        mountainsPage.appendChild(cardContainer);
+        
+        let cardImg = document.createElement("img");
+        cardImg.classList.add("card-img-top");
+        cardImg.classList.add("my-2");
+        cardImg.src = `./images/${mountainArray.img}`;
+        cardImg.alt = mountainArray.name;
+        
+        cardContainer.appendChild(cardImg);
+        
+        let cardBodyDiv = document.createElement("div");
+        cardBodyDiv.classList.add("card-body")
+        cardContainer.appendChild(cardBodyDiv);
+        
+        let cardTitle = document.createElement("h4");
+        cardTitle.innerText = mountainArray.name;
+        cardBodyDiv.appendChild(cardTitle);
+        
+        let cardParagraph = document.createElement("p");
+        cardParagraph.innerText = mountainArray.desc;
+        cardBodyDiv.appendChild(cardParagraph);
+        
+        let cardElevation = document.createElement("p");
+        cardElevation.innerText = `Elevation: ${mountainArray.elevation} ft`
+        cardElevation.classList.add("h5");
+        cardBodyDiv.appendChild(cardElevation);
+
+        
+        
+        // This is the code that works, take out everything else below ==============
+        
+
+        // let cardImg = document.createElement("img");
+        // cardImg.classList.add("card-img-top");
+        // cardImg.src = `./images/${mountainCard.img}`;
+        // cardImg.alt = mountainCard.name;
+        
+        // cardContainer.appendChild(cardImg);
+        
+        // let cardBodyDiv = document.createElement("div");
+        // cardBodyDiv.classList.add("card-body")
+        // cardContainer.appendChild(cardBodyDiv);
+        
+        // let cardTitle = document.createElement("h4");
+        // cardTitle.innerText = mountainCard.name;
+        // cardBodyDiv.appendChild(cardTitle);
+        
+        // let cardParagraph = document.createElement("p");
+        // cardParagraph.innerText = mountainCard.desc;
+        // cardBodyDiv.appendChild(cardParagraph);
+        
+        // let cardElevation = document.createElement("p");
+        // cardElevation.innerText = `Elevation: ${mountainCard.elevation} ft`
+        // cardElevation.classList.add("h5");
+        // cardBodyDiv.appendChild(cardElevation);
+
+    }
+
+
     
-    let cardBodyDiv = document.createElement("div");
-    cardBodyDiv.classList.add("card-body")
-    cardContainer.appendChild(cardBodyDiv);
 
-    let cardTitle = document.createElement("h4");
-    cardTitle.innerText = mountainCard.name;
-    cardBodyDiv.appendChild(cardTitle);
-
-    let cardParagraph = document.createElement("p");
-    cardParagraph.innerText = mountainCard.desc;
-    cardBodyDiv.appendChild(cardParagraph);
-
-    let cardElevation = document.createElement("p");
-    cardElevation.innerText = `Elevation: ${mountainCard.elevation} ft`
-    cardElevation.classList.add("h5");
-    cardBodyDiv.appendChild(cardElevation);
-
+    if (selectedValue == "All"){
+        for (let mountain of mountains){
+            mountainCardCreate(mountain);
+        }
+    }
+    else{
+        mountainCardCreate(mountainCard);
+    }
+        
 }
 
 // ============ DISPLAY CARD IMAGE AND INFORMATION ================
-
-function displayCardInformation(){
-
-
-    
-}
