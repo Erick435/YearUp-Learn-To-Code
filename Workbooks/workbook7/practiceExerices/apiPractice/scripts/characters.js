c2073b289396c196594a9d50e17f570dc1c72527
Erick Ortega <shut435@yahoo.com>
2024-06-06 12:02:57 -0400

    created bootstrap card and shows img/title with selected dropdown

--- Workbooks/workbook7/practiceExerices/apiPractice/scripts/characters.js
+++ Workbooks/workbook7/practiceExerices/apiPractice/scripts/characters.js
@@ -3,11 +3,11 @@
 const cardContainer = document.querySelector("#cardContainer");
 const charactersDropDown = document.querySelector("#characterDropDown");
 
-window.onload = function() {
+window.onload = function () {
 
-displayCharactersDropDown();
+    displayCharactersDropDown();
 
-charactersDropDown.onchange = getCharacterInfo;
+    charactersDropDown.onchange = getCharacterInfo;
 
 }
 
@@ -29,16 +29,51 @@ function displayCharactersDropDown() {
             console.log(characters.results);
         })
 
-    
+
 }
 
 function getCharacterInfo() {
 
+    let selectedCharacter = charactersDropDown.value;
+
     console.log("A character was selected");
 
-    fetch("https://rickandmortyapi.com/api/character")
-        .then((response) => response.json())
-        .then((character) => function() {
+    if (charactersDropDown.selectedIndex >= 0) {
 
-        })
+        let text = charactersDropDown.options[charactersDropDown.selectedIndex].text;
+        let value = selectedCharacter;
+
+
+        fetch("https://rickandmortyapi.com/api/character")
+            .then((response) => response.json())
+            .then((characters) => {
+                let allCharacters = characters.results;
+
+                cardContainer.innerText = ""; //Reset the card container to display 1 character
+
+                // EACH CHARACTER
+                for (let character of allCharacters) {
+
+                    // check and only show the character's info when selected
+                    if (text == character.name){
+
+                        // CREATE AN IMAGE FOR EACH CHARACTER CARD
+                        let characterImage = document.createElement("img");
+                        characterImage.src = character.image;
+                        cardContainer.appendChild(characterImage);
+                        
+                        // CREATE A DIV FOR THE BODY TEXT
+                        let cardBodyDiv = document.createElement("div");
+                        cardContainer.appendChild(cardBodyDiv);
+
+                        // CREATE TITLE FOR THE BODY TEXT
+                        let cardTitle = document.createElement("h5");
+                        cardTitle.classList.add("card-title");
+                        cardTitle.innerText = character.name;
+                        cardBodyDiv.appendChild(cardTitle);
+
+                    }
+                }
+            })
+    }
 }
