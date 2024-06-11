907435c587430aa35cc3a2969f98b9e028fbe673
Erick Ortega <shut435@yahoo.com>
2024-06-11 13:35:12 -0400

    successfully created the post method using js to add to the api

--- Workbooks/workbook8/exercises/web-crud/scripts/create-user.js
+++ Workbooks/workbook8/exercises/web-crud/scripts/create-user.js
@@ -1,10 +1,6 @@
 "use strict";
 
 // get the values that the user inputted for new user
-const fullName = document.querySelector("#fullName").value;
-const userName = document.querySelector("#username").value;
-const userEmail = document.querySelector("#email").value;
-const confirmEmail = document.querySelector("#confirmEmail").value;
 const submitButton = document.querySelector("#submitButton");
 
 window.onload = function () {
@@ -13,26 +9,31 @@ window.onload = function () {
 
 }
 
+function test() {
+    console.log(document.querySelector("#fullName").value);
+}
+
 
 function createNewUser() {
     console.log("createNewUser() called");
 
     let bodyData = {
-        name: fullName,
-        username: userName,
-        email: userEmail
+        name: document.querySelector("#fullName").value,
+        username: document.querySelector("#userName").value,
+        email: document.querySelector("#userEmail").value
     }
 
-    if (userEmail != confirmEmail) {
+    if (userEmail.value != confirmEmail.value) {
         alert("Your Email Does Not Match");
+        
     }
     else {
 
         // call the fetch and POST method to create a new user to the api
-        fetch("http://localhost:3000/users/", {
+        fetch("http://localhost:3000/users", {
             method: "POST",
             body: JSON.stringify(bodyData),
-            header: {
+            headers: {
                 "Content-Type": "application/json"
             }
         })
